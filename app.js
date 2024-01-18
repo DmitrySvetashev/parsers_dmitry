1e14928a72e837eba8f453fe761fc6ace1016ec7
DmitrySvetashev <d.sve@mail.ru>
2024-01-18 17:20:20 +0300

    https

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('config');
+const https = require("https");
 var CronJob = require('cron').CronJob;
 const CronTime = require('cron').CronTime;
 const PORT = config.get('port') || 88;
@@ -11,12 +12,25 @@ app.use(express.static('static'));
 app.use('/api/v1',require('./routes/backend'));
 
 async function start() {
-    // запуск таймера на каждые 6 часов
-    // const timerJob = new CronJob('0 0 */6 * * *',function(){ parserGals.iskraParser() });
-    // timerJob.start();
+    if (process.env.NODE_ENV ==='production') {
+        // запуск таймера на каждые 6 часов
+        // const timerJob = new CronJob('0 0 */6 * * *',function(){ parserGals.iskraParser() });
+        // timerJob.start();
 
-    // слушаем команду
-    app.listen(PORT, () => console.log('server runing on ',PORT,' port'));
+        // запуск сервера
+        https
+        .createServer(
+            {
+                key: fs.readFileSync(`/etc/letsencrypt/live/${config.get('serverUrl')}/privkey.pem`),
+                cert: fs.readFileSync(`/etc/letsencrypt/live/${config.get('serverUrl')}/fullchain.pem`),
+            },
+            app
+        )
+        .listen(PORT, () => {
+            console.log('production server runing on ',PORT,' port');
+        });
+    }
+    else app.listen(PORT, () => console.log('developer server runing on ',PORT,' port'));
 }
 
 start();
